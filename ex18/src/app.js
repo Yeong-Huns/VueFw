82903790b5fea1a9394ecc0a6fac3dfd21ce9a79
unknown <vosxja1@gmail.com>
2024-06-04 21:37:29 +0900

    0604

---
+++ ex18/src/app.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    methods: {
+        getData: function (){
+            axios.get('https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json')
+                .then(function (response){
+                    console.log(response);
+                });
+        }
+    }
+});
